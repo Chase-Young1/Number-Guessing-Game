34b006520f6b5741ad1cf45ffde0698f7e5a8c63
Chase Young <chase.young1@outlook.com>
2024-06-14 23:15:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int winningNum = random.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a random number between 1 and 100: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess > winningNum)
+                {
+                    Console.WriteLine("Too high, try again.");
+                }
+                else if (guess < winningNum)
+                {
+                    Console.WriteLine("Too low, try again.");
+                }
+                else
+                {
+                    guess = winningNum;
+                    Console.WriteLine("Congratulations, you guessed correctly!!!");
+                    win = true;
+                }
+                Console.WriteLine();
+            }
+            while (win == false);
+
+            Console.WriteLine("Press any key to close the game.");
+        }
+    }
+}
